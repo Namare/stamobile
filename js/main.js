c4bd6e26f269b79ca841c69e60c6020a54c88ff4
Namare <ootn@rambler.ru>
2017-08-18 13:25:25 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,91 @@
+$(function(){   
+
+            mapCanvas = document.getElementById("map");
+            var myCenter=new google.maps.LatLng(48.5,35);
+            var mapOptions = {
+                center: myCenter,            
+                zoom: 17,
+                zoomControl: false,
+                scaleControl: false,
+                disableDefaultUI: true
+            };
+
+            addMAP = new google.maps.Map(mapCanvas, mapOptions);             
+            var image = 'http://sta.namgam.com/img/marker.png';
+
+            var marker = new google.maps.Marker({
+                    position:  {lat: 0, lng:0},
+                    map: addMAP,                    
+                    icon:image                 
+                });
+
+
+            $('.set_markers').on('click',function(){
+              $('.markers_menu').toggle();
+            });
+
+           
+            current_marker = null;
+            $('.marker_icon').on('click',function(){
+                current_marker = {
+                    id:$(this).data('id'),
+                    name:$(this).data('name')
+                };
+                $('tr.mid').css({"background-color":"#fff"});
+                $('tr.mid'+current_marker.id+'').css({"background-color":"#ececec"})
+            });
+            addMAP.addListener('click',function(e) {
+                $('.markers_menu').hide();
+                if(current_marker!=null){
+                    var new_marker = new google.maps.Marker({
+                        position:  e.latLng,
+                        map: addMAP,
+                        animation: google.maps.Animation.DROP,
+                        icon:'http://sta.namgam.com/icon/'+current_marker.name+'.png'
+                    });
+
+                    $.ajax({
+                        url:"http://sta.namgam.com/map/add_marker_app",
+                        method:'POST',
+                        data:'k='+STA.key+"&id="+current_marker.id+"" +
+                            "&lat="+e.latLng.lat()+
+                            "&lng="+e.latLng.lng()
+                    });
+                    current_marker = null;
+                    
+                }
+            });
+
+  setInterval( start_geo, 3000); 
+
+  var markers_visible = 0;
+  $('.hide_markers').on('click',function(){
+    if(markers_visible == 0){
+      $('#markers_script').load( "http://sta.namgam.com/map/app_markers"); 
+      markers_visible =1; 
+    }else{
+       for (var i = 0; i < map_markers.length; i++) {           
+           map_markers[i].setMap(null);
+        }
+        markers_visible = 0;
+
+    }
+    
+  });
+ 
+  function start_geo () {
+
+  if ( navigator.geolocation ) {
+     navigator.geolocation.getCurrentPosition(function(p){
+          
+          marker.setPosition({lat: p.coords.latitude, lng:p.coords.longitude});            
+          addMAP.setCenter(marker.getPosition());
+
+     },function(err){
+          //  $('body').text(err.message+err.code);
+     },{ enableHighAccuracy: true});     
+    } 
+  } 
+
+
+});
---
+++ js/sta.js
@@ -0,0 +1,341 @@
+STA = {
+	init:function(){
+		STA.decor();
+		STA.login();
+		STA.stamenu();
+		STA.radio();
+		STA.menuHide();
+		$('.page[data-id="login"]').show();
+
+		$('.log_out').on('click',function(){
+			window.localStorage.removeItem('prem_key');
+			location.reload();
+		});
+		
+		
+	},
+	decor:function(){
+
+		$('.menu-item-close').on('click',STA.menuHide);
+
+		$('.open_menu').on('click',function(){
+			if($('.left_menu').is(':visible')==false){
+				$('.left_menu').show();
+				$('.left_menu').animate({left:0},400);
+			}else{
+				STA.menuHide();
+			}				
+
+		});
+
+		$('body').on('click','.item_order',function(){
+			$(this).next('.item_order_info').slideToggle('fast');
+
+		});
+
+
+	},
+	currentPage:'login',
+	forumCounter:0,
+	prevForum:'',
+	stamenu:function(){
+		$('div.menu-item, .bottom_menu_item').on('click',function(){
+			$('.return_menu').unbind('click');
+			$('.getOrder').unbind('click');
+			 $('.markers_menu').hide();
+
+			prevForum = '';		
+			forumCounter = 0;
+			$('.return_menu').hide();
+			$('.page').hide();
+			
+			var pageName =  $(this).data('id');
+			if(STA.key_status==false){
+					pageName = 'login';
+			}			
+			currentPage = $('.page[data-id="'+pageName+'"]');
+			currentPage.show();
+			STA.menuHide();
+
+
+			switch (pageName){
+				case 'map':
+					google.maps.event.trigger(mapCanvas, "resize");
+				break;
+
+				case 'disp':
+					 
+					 $.ajax({
+					 	method:"POST",
+					 	dataType: 'html',
+					 	url:'http://sta.namgam.com/user',
+					 	data:'k='+STA.key,
+					 	success:function(h){					 		
+					 		
+					 		var div = $(h).find('#app_list_dispach').html();
+        					currentPage.html(div);
+        					$('#app_span5').attr('colspan','5');
+        					$('.load_addition, .load_docs').on('click',function(){
+        						window.open('http://sta.namgam.com/user', '_system');
+        					});
+					 	}
+					 });
+
+				break;
+				case 'menu1':
+				  currentPage.load('http://sta.namgam.com/logistic .list_order_content',function(){
+				  	var BASE_URL = 'http://sta.namgam.com/logistic';
+
+				  	 $('.getOrder').on('click',function(){
+				  	 	$('.alert-success').remove();
+
+             			$('#getorderModal').modal();
+             			$('#getorderModal').find('.orderName').text($(this).data('order_id'));
+             			$('#getorderModal').find('#sendOrder').attr('rel',$(this).data('order_id'));
+             			$('#getorderModal').find('#sendOrder').data('owner_id',$(this).data('owner_id'));
+
+             			$('.get_phone span').load(BASE_URL+'/get_phone/'+$(this).data('owner_id'));
+             			$('.get_email span').load(BASE_URL+'/get_email/'+$(this).data('owner_id'));
+             			$('.get_name span').load(BASE_URL+'/get_name/'+$(this).data('owner_id'));
+
+             			 $.ajax({
+             			     method:"POST",
+             			     url:BASE_URL+"/get_order",
+               				 data:'k='+STA.key+'&order_id='+$(this).data('order_id') + '&owner_id='+$(this).data('owner_id'),
+               				 success:function(b){
+               				 	$('#getorderModal').find('.container-fluid').prepend('<div class="alert alert-success"><strong>'+b+'</strong></div>');                     			
+               				 }
+              			});
+        			 });
+
+						$('.app_trim').each(function(){
+							var name_road = $(this).text().split(', ');
+							$(this).text(name_road[0]+', '+name_road[1]);
+						});	
+						$('.add_arr_app').append(' <i class="fa fa-long-arrow-right" aria-hidden="true"></i>');
+        			  $('#sendOrder').on('click',function(){
+        			  	$('.alert-success').remove();
+
+             			$.ajax({
+                 			method:"POST",
+                 			url:BASE_URL+"/get_order",
+                 			data:'k='+STA.key+'&order_id='+$(this).attr('rel')+'&owner_id='+$(this).data('owner_id')+
+                     			'&commentOrder='+$("textarea[name='commentOrder']").val(),
+                 			success:function(){
+                     			$('#getorderModal').find('.container-fluid').prepend('<div class="alert alert-success"><strong>You got new load!</strong> Your`s messege send.</div>');                     			
+                     			$("textarea[name='commentOrder']").val();
+                     			
+                 			}
+             			});
+        		 	});
+
+				  });
+				break;
+
+				case 'radio':
+				  currentPage.load('http://sta.namgam.com/radio');
+				break;
+
+				case 'forum':
+				currentPage.off('click');
+				 	$('.return_menu').on('click',function(){
+				 		if(prevForum!==''){		
+							if(forumCounter==1){
+								currentPage.load('http://sta.namgam.com/forum .app_list_forums',STA.selectForum);		
+								$('.return_menu').hide();
+							}
+							if(forumCounter==2){
+								forumCounter = 1;
+								currentPage.load(prevForum +' .app_list_forums',STA.selectSubForum);
+							}						
+						}					
+					});							 
+				  	currentPage.load('http://sta.namgam.com/forum .app_list_forums',STA.selectForum);
+				  	
+
+				  	currentPage.on('click','.app_msg_send',function(){
+				  	 var th = $(this);
+				  		$.ajax({
+				  			method:'POST',
+				  			url:'http://sta.namgam.com/forum/new_comment',
+				  			data:'k='+STA.key+'&id='+$(this).data('id')+'&cm='+$('.app_msg').val(),
+				  			success:function(){
+				  				$('.app_msg').val('');
+				  				currentPage.load('http://sta.namgam.com/forum/view_thread/'+$('.app_msg_send').data('id') +' .app_list_forums',STA.selectSubForum);
+				  				$('html, body').animate({
+        							scrollTop: $(document).height()
+   								}, 2000);
+   								th.off('click');
+				  			}
+				  		});
+				  	});
+
+
+				break;
+			}
+
+		});
+	
+
+	},
+	selectSubForum:function(){
+		
+		
+		STA.deleteLinks();	
+		 	
+		$('.sub_forum_href').on('click',function(){	
+				forumCounter = 2;		  			
+			currentPage.load($(this).data('target')+' .app_list_forums',function(){
+	   		STA.deleteLinks();
+		  		$('.return_menu').show();		  						  			 
+			});
+  		});	
+	},
+	selectForum:function(){		
+	
+		STA.deleteLinks();	
+
+		$('.forum_href').on('click',function(){	
+			forumCounter = 1;
+		
+		  	prevForum = $(this).data('target');	
+		  
+			currentPage.load($(this).data('target')+' .app_list_forums',function(){
+	   		STA.deleteLinks();
+		  	$('.return_menu').show();
+		  		STA.selectSubForum();				  			 
+			});
+  		});	
+	},
+	deleteLinks:function(){		
+		$('a').each(function(){
+				  $(this).attr('href','javascript:void(0)');
+		});
+	},
+	radio:function(){
+		$('.radio_play_btn').on('click',function(){
+
+
+			if($('#radio_player')[0].paused){
+				document.getElementById('radio_player').play();
+
+				$(this).find('i').removeClass('fa-play');
+				$(this).find('i').addClass('fa-pause');
+			}
+			else{
+				document.getElementById('radio_player').pause();			
+				$(this).find('i').addClass('fa-play');
+				$(this).find('i').removeClass('fa-pause');
+			}
+		});
+		$('.radio_hide_btn').on('click',function(){$('.radio_player').slideUp()});
+
+		$('body').on('click','.radio-block',function(){
+			$('.radio_player').slideDown();
+			var th = $(this);
+		
+			
+			setTimeout(function () {    	
+			$('.radio_name').text(th.find('.radio-head').text());
+			$('#radio_player').attr('src',th.data('audio'));	
+				if($('#radio_player')[0].paused){		
+					document.getElementById('radio_player').play();
+				}		
+			},500);
+
+		});
+
+	},
+	menuHide:function(){
+				$('.left_menu').animate({left:'-'+$('.left_menu').width()},400);
+				$('.left_menu').hide(100);	
+	},
+	get_user_data:function(key){
+			if (key == undefined)
+				key=STA.key;
+		
+	
+			$.ajax({
+				url:STA.ajax_url,
+				method:'POST',
+				dataType : "json",
+				data:'k='+key,
+				success:function(b){
+					if(b.user_name == false){
+						$('.login_block').show();
+						return false;
+					}
+					STA.key_status = true;
+					$('.user_data').slideDown();
+					$('.user_name').text(b.user_name);
+				}  
+
+			});
+	},
+	key:window.localStorage.getItem("prem_key"),
+	key_status:false,
+	ajax_url:'http://sta.namgam.com/auth/app_login',
+	base_url:'http://sta.namgam.com/',
+	login:function(){
+			
+
+		if(STA.key!==undefined){
+			$('.login_block').hide();
+			STA.get_user_data();
+		}
+
+
+		$('.send_auth').on('click',function(){
+			var login = $('.auth_login').val();
+			var pass = $('.auth_pass').val();
+			$.ajax({
+				method:'POST',
+				dataType : "json",  
+				url:STA.ajax_url,
+				data:'i='+login+'&p='+pass,
+				success:function(d){
+					if(d.err){						
+						window.localStorage.setItem("prem_key", d.key);
+						STA.key = window.localStorage.getItem("prem_key");
+						STA.key_status = true;
+						
+						$('.login_block').slideUp();
+						$('.user_data').slideDown();
+						STA.get_user_data(d.key);
+						STA.load_status();
+					}else{
+						$('.form_error').text(d.err);
+					}
+					$('.form_error').slideDown('slow');
+
+				}
+			})
+		});
+
+		$('.change_status').on('change',function(){
+			$.ajax({
+			method:'POST',
+			url:STA.base_url+'auth/app_change_status',
+			data:'k='+STA.key+'&s='+$(this).val()
+			});
+		});
+		STA.load_status();
+
+		
+		
+
+	},
+	load_status:function(){
+		$.ajax({
+			method:'POST',
+			url:STA.base_url+'auth/app_get_status',
+			data:'k='+STA.key,
+				success:function(b){
+					$('.change_status').val(b);
+				}
+			});
+	}
+}
+$(function(){
+STA.init();
+});
