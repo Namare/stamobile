1b674cddaac67f92e48e2d5bbd0f500b648d450e
Namare <ootn@rambler.ru>
2017-09-12 22:03:53 +0300

    1

--- js/sta.js
+++ js/sta.js
@@ -417,7 +417,7 @@ STA = {
             $('#markers_dist_script').load( "https://stassociation.com/map/app_markers_dist");
             for (var i = 0; i < map_markers_dist.length; i++) {
 
-                if(STA.count_alert.indexOf(map_markers_dist[i].id)){
+                if(STA.count_alert.indexOf(map_markers_dist[i].id) != -1){
                     break;
                 }
                 var myLatLng = new google.maps.LatLng(parseFloat(map_markers_dist[i].position.lat()),parseFloat(map_markers_dist[i].position.lng()));
