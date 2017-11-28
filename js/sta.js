f611baba5fac13928a7a9f921d5fb643cb857799
Namare <ootn@rambler.ru>
2017-11-28 18:10:39 +0200

    1

--- js/sta.js
+++ js/sta.js
@@ -14,8 +14,11 @@ STA = {
 			window.localStorage.removeItem('prem_key');
 			location.reload();
 		});
-		
-	},
+
+
+
+
+    },
 	decor:function(){
 
 		$('.menu-item-close').on('click',STA.menuHide);
@@ -255,11 +258,10 @@ STA = {
 					});							 
 				  	currentPage.load('https://stassociation.com/forum .app_list_forums',STA.selectForum);
 
-				  	currentPage.on('change','#app_mic',function(){
-                        $( ".app_img_send" ).hide();
-                        $( ".app_audio_send" ).show();
-                    });
-				  	currentPage.on('change','#app_cam',function(){
+
+				  	currentPage.on('click','.app_mic', toggleRecording());
+
+                    currentPage.on('change','#app_cam',function(){
                         $( ".app_audio_send" ).hide();
                         $( ".app_img_send" ).show();
 
@@ -327,9 +329,7 @@ STA = {
                     });
 
 
-				  	currentPage.on('click','.app_mic',function(){
-                        $( "#app_mic" ).trigger( "click" );
-                    });
+
 
 	                currentPage.on('click','.app_cam',function(){
                         $( "#app_cam" ).trigger( "click" );
@@ -392,7 +392,255 @@ STA = {
                             }
 				  		});
 				  	});
-				break;
+
+
+                    formData = new FormData($('.forum_audio')[0]);
+
+                    var WORKER_PATH = 'recorderWorker.js';
+
+                    var Recorder = function(source, cfg){
+                        var config = cfg || {};
+                        var bufferLen = config.bufferLen || 4096;
+                        this.context = source.context;
+                        if(!this.context.createScriptProcessor){
+                            this.node = this.context.createJavaScriptNode(bufferLen, 2, 2);
+                        } else {
+                            this.node = this.context.createScriptProcessor(bufferLen, 2, 2);
+                        }
+
+                        var worker = new Worker(config.workerPath || WORKER_PATH);
+                        worker.postMessage({
+                            command: 'init',
+                            config: {
+                                sampleRate: this.context.sampleRate
+                            }
+                        });
+                        var recording = false,
+                            currCallback;
+
+                        this.node.onaudioprocess = function(e){
+                            if (!recording) return;
+                            worker.postMessage({
+                                command: 'record',
+                                buffer: [
+                                    e.inputBuffer.getChannelData(0),
+                                    e.inputBuffer.getChannelData(1)
+                                ]
+                            });
+                        }
+
+                        this.configure = function(cfg){
+                            for (var prop in cfg){
+                                if (cfg.hasOwnProperty(prop)){
+                                    config[prop] = cfg[prop];
+                                }
+                            }
+                        }
+
+                        this.record = function(){
+                            recording = true;
+                        }
+
+                        this.stop = function(){
+                            recording = false;
+                        }
+
+                        this.clear = function(){
+                            worker.postMessage({ command: 'clear' });
+                        }
+
+                        this.getBuffers = function(cb) {
+                            currCallback = cb || config.callback;
+                            worker.postMessage({ command: 'getBuffers' })
+                        }
+
+                        this.exportWAV = function(cb, type){
+                            currCallback = cb || config.callback;
+                            type = type || config.type || 'audio/wav';
+                            if (!currCallback) throw new Error('Callback not set');
+                            worker.postMessage({
+                                command: 'exportWAV',
+                                type: type
+                            });
+                        }
+
+                        this.exportMonoWAV = function(cb, type){
+                            currCallback = cb || config.callback;
+                            type = type || config.type || 'audio/wav';
+                            if (!currCallback) throw new Error('Callback not set');
+                            worker.postMessage({
+                                command: 'exportMonoWAV',
+                                type: type
+                            });
+                        }
+
+                        worker.onmessage = function(e){
+                            var blob = e.data;
+                            currCallback(blob);
+                        }
+
+                        source.connect(this.node);
+                        this.node.connect(this.context.destination);   // if the script node is not connected to an output the "onaudioprocess" event is not triggered in chrome.
+                    };
+
+                    Recorder.setupDownload = function(blob, filename){
+                        var url = (window.URL || window.webkitURL).createObjectURL(blob);
+                        var link = document.getElementById("save");
+
+
+                        formData.append("recording",blob,filename);
+                        console.log(formData);
+                        console.log(formData.recording);
+                        $.ajax({
+                            url:'https://stassociation.com/logistic/upload_app',
+                            type: 'POST',
+                            data: formData,
+                            processData: false,
+                            contentType: false
+
+                        });
+
+                        link.href = url;
+
+                        link.download = filename || 'output.wav';
+                    }
+
+                    window.Recorder = Recorder;
+
+                    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+                    var audioContext = new AudioContext();
+                    var audioInput = null,
+                        realAudioInput = null,
+                        inputPoint = null,
+                        audioRecorder = null;
+                    var rafID = null;
+                    var analyserContext = null;
+                    var canvasWidth, canvasHeight;
+                    var recIndex = 0;
+
+                    /* TODO:
+
+                     - offer mono option
+                     - "Monitor input" switch
+                     */
+
+                function saveAudio() {
+                    audioRecorder.exportWAV( doneEncoding );
+                    // could get mono instead by saying
+                    // audioRecorder.exportMonoWAV( doneEncoding );
+                }
+
+                function gotBuffers( buffers ) {
+
+
+                    // the ONLY time gotBuffers is called is right after a new recording is completed -
+                    // so here's where we should set up the download.
+                    audioRecorder.exportWAV( doneEncoding );
+                }
+
+                function doneEncoding( blob ) {
+                    Recorder.setupDownload( blob, "myRecording" + ((recIndex<10)?"0":"") + recIndex + ".wav" );
+                    recIndex++;
+                }
+
+                function toggleRecording( e ) {
+                    if (e.classList.contains("recording")) {
+                        // stop recording
+                        audioRecorder.stop();
+                        e.classList.remove("recording");
+                        audioRecorder.getBuffers( gotBuffers );
+
+                    } else {
+                        // start recording
+                        if (!audioRecorder)
+                            return;
+                        e.classList.add("recording");
+                        audioRecorder.clear();
+                        audioRecorder.record();
+
+                    }
+                }
+
+                function convertToMono( input ) {
+                    var splitter = audioContext.createChannelSplitter(2);
+                    var merger = audioContext.createChannelMerger(2);
+
+                    input.connect( splitter );
+                    splitter.connect( merger, 0, 0 );
+                    splitter.connect( merger, 0, 1 );
+                    return merger;
+                }
+
+                function cancelAnalyserUpdates() {
+                    window.cancelAnimationFrame( rafID );
+                    rafID = null;
+                }
+
+
+                function toggleMono() {
+                    if (audioInput != realAudioInput) {
+                        audioInput.disconnect();
+                        realAudioInput.disconnect();
+                        audioInput = realAudioInput;
+                    } else {
+                        realAudioInput.disconnect();
+                        audioInput = convertToMono( realAudioInput );
+                    }
+
+                    audioInput.connect(inputPoint);
+                }
+
+                function gotStream(stream) {
+                    inputPoint = audioContext.createGain();
+                    realAudioInput = audioContext.createMediaStreamSource(stream);
+                    audioInput = realAudioInput;
+                    audioInput.connect(inputPoint);
+
+
+                    analyserNode = audioContext.createAnalyser();
+                    analyserNode.fftSize = 2048;
+                    inputPoint.connect( analyserNode );
+
+                    audioRecorder = new Recorder( inputPoint );
+
+                    zeroGain = audioContext.createGain();
+                    zeroGain.gain.value = 0.0;
+                    inputPoint.connect( zeroGain );
+                    zeroGain.connect( audioContext.destination );
+                    zeroGain.gain.value = 1;
+
+                }
+
+                function initAudio() {
+                    if (!navigator.getUserMedia)
+                        navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+                    if (!navigator.cancelAnimationFrame)
+                        navigator.cancelAnimationFrame = navigator.webkitCancelAnimationFrame || navigator.mozCancelAnimationFrame;
+                    if (!navigator.requestAnimationFrame)
+                        navigator.requestAnimationFrame = navigator.webkitRequestAnimationFrame || navigator.mozRequestAnimationFrame;
+
+                    navigator.getUserMedia(
+                        {
+                            "audio": {
+                                "mandatory": {
+                                    "googEchoCancellation": "false",
+                                    "googAutoGainControl": "false",
+                                    "googNoiseSuppression": "false",
+                                    "googHighpassFilter": "false"
+                                },
+                                "optional": []
+                            }
+                        }, gotStream, function(e) {
+                            alert('Error getting audio');
+                            console.log(e);
+                        });
+                }
+
+
+
+
+                    break;
             }
 		});
 	},
