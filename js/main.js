1b11fc3862bcbea8270efcdbe19d9e03a962024e
Namare <nwebootn@gmail.com>
2018-07-31 22:41:07 +0300

    alert

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function initMap (){
     window.alert = function (txt) {
         navigator.notification.alert(txt, null, "Alert", "Close");
     }
+    cordova.plugins.backgroundMode.enable();
     icon_size_w =60;
     icon_size_h =60;
     all_drivers = [];
